afa97513140eaa80689fcf53a7211cc3dbeae4e0
kajugadaniels <kajugadaniels@gmail.com>
2024-04-01 10:41:22 +0200

    Initial release

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from 'antd';
+
+export default function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+
+      <Button type="primary">Button</Button>
+    </>
+  )
+}
