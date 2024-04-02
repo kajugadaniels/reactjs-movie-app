a2ff7af17a10d43e4dea76fa607c3ab15a650dc7
kajugadaniels <kajugadaniels@gmail.com>
2024-04-02 20:12:28 +0200

    Initialize API in Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,7 +20,25 @@ function App() {
         <Hero />
 
         <div className="w-full">
-          
+          <div className="w-full flex items-center my-6">
+            <div className="text-lg mr-3 inline-flex lg:text-4xl w-full font-bold">
+              <span className="flex items-center text-2xl">
+                <img src={images.fireIc} alt="" className="h-10 w-16" />
+                Trending
+              </span>
+            </div>
+            <div className="inline-flex h-[1px] w-[9999px] bg-linesColor"></div>
+            <div className="inline-flex px-12">See&nbsp;More</div>
+          </div>
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6 gap-10">
+            {movies.map((movie, index) => (
+              <Card
+                key={movie.id}
+                item={movie}
+              />
+            ))}
+          </div>
         </div>
       </div>
     );
